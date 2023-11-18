9da8463edd3279beabf2b15e33f1f3da0b0b95dc
rfrenchy <ryanfrench@outlook.com>
2023-11-18 09:33:30 +0000

    format

--- clients/postgres.go
+++ clients/postgres.go
@@ -24,82 +24,85 @@ import (
   **********************************************
 */
 func main() {
-  // Establish DB connection
-  cs := "postgresql://localhost/horse_racing?sslmode=disable"
-  db, err := sql.Open("postgres", cs)
+     
+        // Establish DB connection
+        cs := "postgresql://localhost/horse_racing?sslmode=disable"
+        db, err := sql.Open("postgres", cs)
 
-  if err != nil {
-    panic(err)
-  }
+        if err != nil {
+                panic(err)
+        }
 
-  // Read horse_racing data CSV
-  d, err := os.ReadFile("./lingfield_2023_flat_no_title.csv")
-  if err != nil {
-    panic(err)
-  }
+        // Read CSV into var
+        d, err := os.ReadFile("./lingfield_2023_flat_no_title.csv")
+        if err != nil {
+                panic(err)
+        }
   
-  r := csv.NewReader(strings.NewReader(string(d)))
-  ri := 0
-  for {
-    fmt.Println("Processing row", ri)
-    // Read CSV data row
-    record, err := r.Read()
-    if err == io.EOF {
-      break;
-    }
-    if err != nil {
-      panic(err)
-    }
+        r := csv.NewReader(strings.NewReader(string(d)))
+        ri := 0
 
-    // Create a Transaction
-    tx, err := db.Begin()
-    if err != nil {
-      panic(err)
-    }
+        // Read each line of CSV
+        for {
+                fmt.Println("Processing row", ri)
+                
+                record, err := r.Read()
+                if err == io.EOF {
+                        break;
+                }
+                if err != nil {
+                        panic(err)
+                }
 
-    var sb strings.Builder
-    p := make([]interface{}, len(record)) // can optimise by only working out len(record) once
-    for i, x := range record {
-      // create param string for SQL i.e. $1, $2, $3...
-      sb.WriteString("$" + strconv.Itoa(i + 1) + ",")      
+                // Create a Transaction
+                tx, err := db.Begin()
+                if err != nil {
+                        panic(err)
+                }
 
-      if i == 31 && (x == "" || x == "–") {
-        p[i] = 0 // convert prize column data to 0 if there is no prize
-      } else if i == 33 && x == "–" {
-        p[i] = 0 // convert RPR to 0 if "-" assessment i.e. no assessment score
-      } else if i == 17 && x == "PU" {
-        p[i] = -1 // use -1 to resemble Pulled Up (PU) finish (deliberate stop by jockey)
-      } else if i == 19 && x == "-" {
-        p[i] = 0
-      } else if i == 20 && x == "-" {
-        p[i] = 0
-      } else if i == 26 && x == "-" {
-        p[i] = nil
-      } else if i == 27 && x == "-" {
-        p[i] = nil
-      } else {
-        p[i] = x
-      }
-    }
+                var sb strings.Builder
+                p := make([]interface{}, len(record)) // can optimise by only working out len(record) once
 
-    v := strings.TrimRight(sb.String(), ",") // remove trailing comma
+                for i, x := range record {
+                        // create param string for SQL i.e. $1, $2, $3...
+                        sb.WriteString("$" + strconv.Itoa(i + 1) + ",")      
 
-    // Execute INSERT statement into lingfield table
-    st := "INSERT INTO lingfield VALUES(" + v + ");"
+                        if i == 31 && (x == "" || x == "–") {
+                                p[i] = 0 // convert prize column data to 0 if there is no prize
+                        } else if i == 33 && x == "–" {
+                                p[i] = 0 // convert RPR to 0 if "-" assessment i.e. no assessment score
+                        } else if i == 17 && x == "PU" {
+                                p[i] = -1 // use -1 to resemble Pulled Up (PU) finish (deliberate stop by jockey)
+                        } else if i == 19 && x == "-" {
+                                p[i] = 0
+                        } else if i == 20 && x == "-" {
+                                p[i] = 0
+                        } else if i == 26 && x == "-" {
+                                p[i] = nil
+                        } else if i == 27 && x == "-" {
+                                p[i] = nil
+                        } else {
+                                p[i] = x
+                        }
+                }
 
-    _, err = tx.Exec(st, p...)
-    if err != nil {
-      _ = tx.Rollback()
-      panic(err)
-    }
+                v := strings.TrimRight(sb.String(), ",") // remove trailing comma
 
-    if err := tx.Commit(); err != nil {
-      panic(err)
-    }
+                // Execute INSERT statement into lingfield table
+                st := "INSERT INTO lingfield VALUES(" + v + ");"
 
-    ri++;
-    // Repeat for rest of CSV
-  }
+                _, err = tx.Exec(st, p...)
+                if err != nil {
+                        _ = tx.Rollback()
+                        panic(err)
+                }
 
-  db.Close()
+                if err := tx.Commit(); err != nil {
+                        panic(err)
+                }
+
+                ri++;
+        }
+
+        db.Close()
 }
