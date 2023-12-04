f0034e15e28d6bb08287c780d25d411568874b44
rfrenchy <ryanfrench@outlook.com>
2023-12-04 14:47:39 +0000

    begin migrate scripts

---
+++ cmd/postgres/migrate/001_init.up.sql
@@ -0,0 +1,77 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS horse (
+        horse_id INT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        sex VARCHAR(3) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS trainer (
+        trainer_id INT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS jockey (
+        jockey_id INT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS owner (
+        owner_id INT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        silk_url VARCHAR(500)
+);
+
+CREATE TABLE IF NOT EXISTS region (
+        region_id INT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS course (
+        course_id INT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        region_id INT REFERENCES region (region_id)
+);
+
+CREATE TABLE IF NOT EXISTS race {
+        race_id INT PRIMARY KEY,
+        name VARCHAR(255),
+        'date' DATE,
+        course_id INT REFERENCES course (course_id),
+        off_time TIMESTAMP,
+        race_type VARCHAR(255),
+        race_class VARCHAR(255),
+        pattern VARCHAR(255),
+        rating_band VARCHAR(255),
+        age_band_restriction VARCHAR(255),
+        sex_restriction VARCHAR(255),
+        distance REAL,
+        going VARCHAR(255),
+        surface VARCHAR(255),
+        ran INT
+}
+
+CREATE TABLE IF NOT EXISTS runner {
+        horse_id INT REFERENCES horse (horse_id)
+        race_id INT REFERENCES race (race_id)
+        race_card_number INT,
+        position INT,
+        draw INT,
+        over_beaten INT,
+        beaten INT,
+        age VARCHAR(255),
+        weight VARCHAR(255),
+        headgear VARCHAR(255),
+        time TIMESTAMP,
+        odds REAL,
+        jockey_id INT REFERENCES jockey (jockey_id),
+        trainer_id INT REFERENCES trainer (trainer_id),
+        prizemoney NUMERIC(10,2),
+        official_rating REAL,
+        rp_rating REAL,
+        ts_rating REAL,
+        owner_id INT REFERENCES owner (owner_id),
+        'comment' TEXT
+}
+
+COMMIT;
