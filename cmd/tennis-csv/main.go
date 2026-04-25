ff08cf2806e30903036c3f26f82b9d25d79327fd
Ryan French <ryanfrench@outlook.com>
2026-04-25 10:38:01 +0100

    id primary key

--- cmd/tennis-csv/main.go
+++ cmd/tennis-csv/main.go
@@ -30,6 +30,7 @@ func main() {
 	// 2. Create table
 	createTableSQL := `
 	CREATE TABLE IF NOT EXISTS atp_matches (
+		id SERIAL PRIMARY KEY,
 		tourney_id TEXT,
 		tourney_name TEXT,
 		surface TEXT,
@@ -78,8 +79,7 @@ func main() {
 		winner_rank INT,
 		winner_rank_points INT,
 		loser_rank INT,
-		loser_rank_points INT,
-		PRIMARY KEY (tourney_id, match_num)
+		loser_rank_points INT		
 	);`
 
 	_, err = db.Exec(createTableSQL)
@@ -127,7 +127,7 @@ func main() {
 			$28, $29, $30, $31, $32, $33, $34, $35, $36,
 			$37, $38, $39, $40, $41, $42, $43, $44, $45,
 			$46, $47, $48, $49
-		) ON CONFLICT (tourney_id, match_num) DO NOTHING;`
+		) ON CONFLICT (id) DO NOTHING;`
 
 		tx, err := db.Begin()
 		if err != nil {
